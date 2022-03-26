e00f00080f66fd42f853f9a2b95c05ffac5874f9
Kriti Singh <90671322+Kriti0802@users.noreply.github.com>
2022-03-26 11:59:37 +0530

    Add files via upload

---
+++ Palindromic_string.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	string s,rev;
+	cin>>s;
+
+	for(int i=s.length()-1;i>=0;i--){
+		rev =rev+s[i];
+	}
+	if(s==rev){
+		cout<<"YES"<<endl;
+	}
+	else{
+		cout<<"NO"<<endl;
+	}
+	
+	
+	
+	
+}
